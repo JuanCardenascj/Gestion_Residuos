6fe2a6fd10097a3925610017eae4b96c83faab34
JuanCarDevs <jdcj123@hotmail.com>
2025-04-21 21:20:02 -0500

    Add files via upload

---
+++ storage/framework/views/20f1c1667e3c06f9e3e5b922df813212.php
@@ -0,0 +1,33 @@
+
+
+<?php $__env->startSection('content'); ?>
+<div class="container" id="login-page">
+    <div class="header">
+        <!--<img src="<?php echo e(asset('images/logo.png')); ?>" alt="logo" class="logo">-->
+        <span>ARDI-MI ♻️</span>
+    </div>
+    <h1>Gestión de Residuos</h1>
+
+    <div id="form-section">
+        <h2>Iniciar Sesión</h2>
+        <form method="POST" action="<?php echo e(route('login')); ?>">
+            <?php echo csrf_field(); ?>
+            <input type="email" id="loginEmail" name="email" placeholder="Correo electrónico" required />
+            <input type="password" id="loginPassword" name="password" placeholder="Contraseña" required />
+
+            <div class="whatsapp-option">
+                <input type="checkbox" id="whatsappNotification" name="whatsapp_notification">
+                <label for="whatsappNotification">Recibir notificaciones por WhatsApp</label>
+            </div>
+
+            <button type="submit">Ingresar</button>
+        </form>
+
+        <div class="login-options">
+            <a href="<?php echo e(route('password.request')); ?>">¿Olvidaste tu contraseña?</a>
+            <a href="<?php echo e(route('register')); ?>">¿Todavía no tienes una cuenta? Regístrate</a>
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.app', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\xampp\htdocs\ardi-mi\resources\views/auth/login.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/825edbb06299b2d03ba94d1758c2eae0.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ARDI-MI ♻️ <?php echo $__env->yieldContent('title'); ?></title>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <?php echo $__env->yieldPushContent('styles'); ?>
+    <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
+</head>
+<body>
+    <?php echo $__env->yieldContent('content'); ?>
+    <?php echo $__env->yieldPushContent('scripts'); ?>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\ardi-mi\resources\views/layouts/app.blade.php ENDPATH**/ ?>
